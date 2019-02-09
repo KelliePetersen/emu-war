db4b68e3dd7fb999891c8b924a9eb94d87b23e0c
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-02-09 00:04:51 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+require('./gulp/tasks/css');
+require('./gulp/tasks/modernizr');
+require('./gulp/tasks/js');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/build');
+require('./gulp/tasks/export');
