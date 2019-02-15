913bed8a3061566cf50d3d9933aef5603b2b4a63
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-02-15 11:54:48 -0500

    add dynamic nav with waypoints

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,4 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import DynamicNav from './modules/DynamicNav';
 
 const mobileMenu = new MobileMenu();
+const dynamicNav = new DynamicNav();
 // const backToTop = new BackToTop();
---
+++ app/src/js/modules/DynamicNav.js
@@ -0,0 +1,59 @@
+import waypoints from 'waypoints/lib/noframework.waypoints';
+
+class DynamicNav {
+  constructor() {
+    // this.lazyImages = $('.lazyload');
+    this.pageSections = document.getElementsByClassName('section');
+    this.links = document.getElementsByClassName('nav__link');
+    this.createWaypoints();
+    // this.refreshWaypoints();
+  }
+
+  // refreshWaypoints() {
+  //   this.lazyImages.on('load', function () {
+  //     Waypoint.refreshAll();
+  //   });
+  // }
+
+  createWaypoints() {
+    let mainThis = this;
+    for (var i = 0; i < this.pageSections.length; i++) {
+      let currentItem = this;
+      let newItem = currentItem.pageSections[i];
+      new Waypoint({
+        element: mainThis.pageSections[i],
+        handler: function (direction) {
+          if (direction == 'down') {
+            let matchingLink = newItem.getAttribute('data-link');
+
+            for (var j = 0; j < mainThis.links.length; j++) {
+              mainThis.links[j].classList.remove('nav__active-link');
+            }
+
+            let currentLink = document.querySelector(matchingLink);
+            currentLink.classList.add('nav__active-link');
+          }
+        },
+        offset: '20%'
+      });
+      new Waypoint({
+        element: mainThis.pageSections[i],
+        handler: function (direction) {
+          if (direction == 'up') {
+            let matchingLink = newItem.getAttribute('data-link');
+
+            for (var j = 0; j < mainThis.links.length; j++) {
+              mainThis.links[j].classList.remove("nav__active-link");
+            }
+
+            let currentLink = document.querySelector(matchingLink);
+            currentLink.classList.add('nav__active-link');
+          }
+        },
+        offset: '-40%'
+      });
+    };
+  }
+}
+
+export default DynamicNav;
