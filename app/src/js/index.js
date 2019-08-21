6d8bcfcca9f60a4c9170722f5cfef87e28f1a76d
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-08-21 02:10:42 -0400

    move scrollreveal to module

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,36 +1,29 @@
 import MobileMenu from './modules/MobileMenu';
 import DynamicNav from './modules/DynamicNav'; 
 import BackToTop from './modules/BackToTop';
+import RevealonScroll from './modules/RevealonScroll';
 
 const mobileMenu = new MobileMenu();
 const dynamicNav = new DynamicNav();
 const backToTop = new BackToTop();
+const revealonScroll = new RevealonScroll();
 
-const options = {
-  delay: 100,
-  duration: 1000,
-  distance: "100px",
-  easing: "ease-in-out"
-};
-
-let sr = ScrollReveal(options);
-
-sr.reveal(".hero__text", { distance: "10px" });
-sr.reveal(".hero__quote", { distance: "10px", delay: 1500 });
-sr.reveal(".hero__tribute", { distance: "10px", delay: 1500 });
+revealonScroll.sr.reveal(".hero__text", { distance: "10px" });
+revealonScroll.sr.reveal(".hero__quote", { distance: "10px", delay: 1500 });
+revealonScroll.sr.reveal(".hero__tribute", { distance: "10px", delay: 1500 });
 
 if (document.documentElement.clientWidth > 1279 && document.documentElement.clientHeight > 800) {
-  sr.reveal(".intro__image", {
+  revealonScroll.sr.reveal(".intro__image", {
     origin: "right",
     delay: 600,
     viewOffset: { right: -400 }
   });
-  sr.reveal(".final-battle__image", {
+  revealonScroll.sr.reveal(".final-battle__image", {
     origin: "right",
     delay: 600,
     viewOffset: { bottom: -300 }
   });
-  sr.reveal(".scroll-reveal-left", { origin: "left", interval: 300 });
-  sr.reveal(".scroll-reveal-right", { origin: "right", interval: 300 });
-  sr.reveal(".scroll-reveal-top", { interval: 300 });
+  revealonScroll.sr.reveal(".scroll-reveal-left", { origin: "left", interval: 300 });
+  revealonScroll.sr.reveal(".scroll-reveal-right", { origin: "right", interval: 300 });
+  revealonScroll.sr.reveal(".scroll-reveal-top", { interval: 300 });
 }
